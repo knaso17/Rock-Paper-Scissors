501fedf0c44fa6e5576fbff001c7508e77d60ca9
knaso17 <90427458+knaso17@users.noreply.github.com>
2022-06-01 15:44:39 -0500

    winnerCheck function works as expected

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ function computerPlay(){
   }
 }
 
-console.log(computerPlay())
+function winnerCheck(playerSelection, computerSelection){
+  let player = playerSelection.toLowerCase()
+  if(player === computerSelection){
+    return "You tied!"
+  }else if(computerSelection === "paper"){
+    return player === "rock" ? "You lose! Paper beats Rock" : "You win! Scissors beats Paper";
+  }else if(computerSelection === "rock"){
+    return player === "scissors" ? "You lose! Rock beats Scissors" : "You win! Paper beats Rock";
+  }else{
+    return player === "paper" ? "You lose! Scissors beats Paper" : "You win! Rock beats Scissors";
+  }
+}
+
+const playerSelection = "PaPer";
+const computerSelection = computerPlay();
+console.log(winnerCheck(playerSelection, computerSelection));
